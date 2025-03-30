5887cc1068d1d71bd580b4207368d9476689ae89
AnthonyFerrara2898 <anthonyferrara2898@gmail.com>
2025-03-30 12:25:53 +0200

    render esercizio

--- esercizi-react/src/App.jsx
+++ esercizi-react/src/App.jsx
@@ -1,9 +1,8 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-}
+import React from "react";
+import Welcome from "./components/Welcome";
+
+const App = () => {
+  return <Welcome name="John" age={25} />;
+};
 
 export default App;
